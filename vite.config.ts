b54e735949669791450296593013750d22726e3e
Bassam Ahmed <bassaam529@gmail.com>
2025-06-10 16:01:16 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "./src"),
+        '@': '/src',
       },
     },
-    base: mode === "production" ? "/Linkup-Ai-dash/" : "/", // غيّر "Linkup-Ai-dash" لاسم الـ repository بتاعك
+    base: mode === 'production' ? '/Linkup-Ai-dash/' : '/',
+    define: {
+      'import.meta.env': env,
+    },
   };
 });
